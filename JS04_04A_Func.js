5646b5d134c332f94793a0472d2a994739de23da
Dung <onlyad9@gmail.com>
2015-05-31 18:00:03 -0400

    New Update

--- JS04_04A_Func.js
+++ JS04_04A_Func.js
@@ -13,23 +13,23 @@
  * Partie 1: fonction permettant de générer un nombre aléatoire entre un minimum et un maximum donnés en paramètres.
  */
 function getRandIntegerBetweenMinAndMax(iRangeMin, iRangeMax) {
-    return parseInt((Math.random() * (iRangeMax - iRangeMin))) + iRangeMin;
+    return Math.ceil((Math.random() * (iRangeMax - iRangeMin))) + iRangeMin;
 }
 /**
  * BONUS
  * Ecrire une autre fonction pour générer des nombres flottants aléatoires. On doit pouvoir choisir le
  * nombre de chiffre après la virgule.
  */
-function getRandFloatBetweenMinAndMax(fRangeMin, fRangeMax, iDecimal) {
+function getRandFloatBetweenMinAndMax(fRangeMin, fRangeMax, iDecimalSize) {
     var iMultiplicationOf10 = 1;
-    for (iCount = 1; iCount <= iDecimal; iCount++) {
+    for (iCount = 1; iCount <= iDecimalSize; iCount++) {
         iMultiplicationOf10 *= 10;
     }
     var fRandFloat = Math.random();
     fRandFloat = (fRandFloat * (fRangeMax - fRangeMin)) + fRangeMin;
     fRandFloat = fRandFloat * iMultiplicationOf10;
     fRandFloat = Math.round(fRandFloat);
-    fRandFloat = fRandFloat / iMultiplicationOf10;
+    fRandFloat = Math.ceil(fRandFloat / iMultiplicationOf10);
     return fRandFloat;
 }
 
