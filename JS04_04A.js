47532cbf33e6ca202efe6967fab20f781bbeaee7
Dung <onlyad9@gmail.com>
2015-05-31 17:43:02 -0400

    New Update

--- JS04_04A.js
+++ JS04_04A.js
@@ -11,10 +11,24 @@
 /**
  * Partie 1: fonction permettant de générer un nombre aléatoire entre un minimum et un maximum donnés en paramètres.
  */
-document.write("<br> Part 1: Random Number between Min and Max deine by user");
+document.write("<br> Part 1: Random Integer Number between Min and Max define by user");
 document.write("<br>Random Number between Min " + 10 + " and Max " + 20 + " is : " +
-getRandBetweenMinAndMax(10, 20));
+getRandIntegerBetweenMinAndMax(10, 20));
 document.write("<br>Random Number between Min " + 10 + " and Max " + 100 + " is : " +
-getRandBetweenMinAndMax(10, 100));
+getRandIntegerBetweenMinAndMax(10, 100));
 document.write("<br>Random Number between Min " + 1 + " and Max " + 6 + " is : " +
-getRandBetweenMinAndMax(1, 6));
+getRandIntegerBetweenMinAndMax(1, 6));
+
+
+/**    BONUS
+ *
+ * Ecrire une autre fonction pour générer des nombres flottants aléatoires. On doit pouvoir choisir le
+ * nombre de chiffre après la virgule.
+ */
+document.write("<br> Part Bonus 1: Random Float Number between Min and Max define by user");
+document.write("<br>Random Number between Min " + 10.00 + " and Max " + 20.00 + " is : " +
+getRandFloatBetweenMinAndMax(10.00, 20.00, 3));
+document.write("<br>Random Number between Min " + 10.00 + " and Max " + 100.00 + " is : " +
+getRandFloatBetweenMinAndMax(10.00, 100.00, 5));
+document.write("<br>Random Number between Min " + 1 + " and Max " + 6 + " is : " +
+getRandFloatBetweenMinAndMax(1.00, 6.00, 2));
--- JS04_04A_Func.js
+++ JS04_04A_Func.js
@@ -9,7 +9,27 @@
 // nombre de chiffre après la virgule.
 
 
-
-function getRandBetweenMinAndMax(iRangeMin, iRangeMax) {
-    return parseInt((Math.random()* (iRangeMax - iRangeMin))) + iRangeMin;
+/**
+ * Partie 1: fonction permettant de générer un nombre aléatoire entre un minimum et un maximum donnés en paramètres.
+ */
+function getRandIntegerBetweenMinAndMax(iRangeMin, iRangeMax) {
+    return parseInt((Math.random() * (iRangeMax - iRangeMin))) + iRangeMin;
 }
+/**
+ * BONUS
+ * Ecrire une autre fonction pour générer des nombres flottants aléatoires. On doit pouvoir choisir le
+ * nombre de chiffre après la virgule.
+ */
+function getRandFloatBetweenMinAndMax(fRangeMin, fRangeMax, iDecimal) {
+    var iMultiplicationOf10 = 1;
+    for (iCount = 1; iCount <= iDecimal; iCount++) {
+        iMultiplicationOf10 *= 10;
+    }
+    var fRandFloat = Math.random();
+    fRandFloat = (fRandFloat * (fRangeMax - fRangeMin)) + fRangeMin;
+    fRandFloat = fRandFloat * iMultiplicationOf10;
+    fRandFloat = Math.round(fRandFloat);
+    fRandFloat = fRandFloat / iMultiplicationOf10;
+    return fRandFloat;
+}
+
